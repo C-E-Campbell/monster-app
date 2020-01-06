40ab54148e9c4eb03af59863bf6592801b8e7aaf
charles <codelyfewithcharlie@gmail.com>
2020-01-05 21:32:12 -0500

    updates

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import "./card.styles.css";
+
 export const Card = props => {
-  return <div className="card-container">{props.monster.name}</div>;
+  return (
+    <div className="card-container">
+      <img
+        src={`http://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        alt="monster"
+      />
+      <h1>{props.monster.name}</h1>
+      <p>{props.monster.email}</p>
+    </div>
+  );
 };
